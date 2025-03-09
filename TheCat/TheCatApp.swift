86728816b744173d0fd507572349b9cc9582d1db
Pedro Altoé Costa <pedroaltoecosta@gmail.com>
2025-03-09 02:39:39 +0000

    Initial Commit

---
+++ TheCat/TheCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheCatApp.swift
+//  TheCat
+//
+//  Created by Pedro Altoe Costa on 9/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct TheCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
