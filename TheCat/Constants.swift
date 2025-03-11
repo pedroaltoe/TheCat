ffd6dc0d4c5c6e422cbb514e4b2f404a5fa0f517
Pedro Altoé Costa <pedroaltoecosta@gmail.com>
2025-03-11 09:09:47 +0000

    Create Constants.swift

---
+++ TheCat/Constants.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+enum Space {
+    static let small: CGFloat = 6
+    static let medium: CGFloat = 12
+    static let large: CGFloat = 18
+}
+
+enum Constants {
+    enum Image {
+        static let size: CGFloat = 100
+        static let placeHolder = "photo"
+        static let fail = "photo.badge.exclamationmark"
+        static let favorite = "star.fill"
+    }
+    
+    enum Item {
+        static let size: CGFloat = 110
+    }
+    
+    enum Text {
+        static let refresh = "Refresh"
+        static let title = "TheCat"
+    }
+    
+}
