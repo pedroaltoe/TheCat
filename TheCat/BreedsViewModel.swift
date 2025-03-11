e034ec93d40e4e2fca61e7c4c8ab2193e9572776
Pedro Altoé Costa <pedroaltoecosta@gmail.com>
2025-03-11 09:10:54 +0000

    Create BreedsViewModel.swift

---
+++ TheCat/BreedsViewModel.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+final class BreedsViewModel: ObservableObject {
+    
+    @Published private(set) var viewState: BreedsViewState
+    
+    var breeds: [Breed] = []
+    let repository: RepositoryProtocol
+    
+    init() {
+        viewState = .initial
+        repository = Repository()
+    }
+    
+    @MainActor
+    func fetchBreeds() async {
+        viewState = .loading
+        do {
+            breeds = try await repository.fetchBreeds()
+            viewState = .loaded(breeds)
+        } catch {
+            viewState = .error(error.localizedDescription)
+        }
+    }
+    
+    func refresh() {
+        Task {
+            await fetchBreeds()
+        }
+    }
+}
