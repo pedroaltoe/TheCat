a498705b864b99da4a482c931368af50a39c5d77
Pedro Altoé Costa <pedroaltoecosta@gmail.com>
2025-03-17 05:07:47 +0000

    Create Localized.swift

---
+++ TheCat/Resources/Localized.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+enum Localized {
+    static let refresh = String(localized: "Refresh")
+    static let title = String(localized: "TheCat")
+    static let catsListButton = String(localized: "Cats List")
+    static let favouritesButton = String(localized: "Favourites")
+    static let origin = String(localized: "Origin")
+    static let temperament = String(localized: "Temperament")
+    static let description = String(localized: "Description")
+}
