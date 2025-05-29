50d7450e0d98e9cec0e15053bfdc9192c0dab0af
Pedro Altoé Costa <pedroaltoecosta@gmail.com>
2025-05-29 17:27:57 +0100

    Update Breed.swift

--- TheCat/Breeds/Breed.swift
+++ TheCat/Breeds/Breed.swift
@@ -1,16 +1,17 @@
 import Foundation
 
-struct Breed: Decodable, Identifiable {
+struct Breed: Decodable, Equatable, Identifiable {
     let id: String
     let image: BreedImage?
     let name: String
     let temperament: String
     let origin: String
     let description: String
+    let referenceImageId: String
     var isFavorite: Bool?
 }
 
-struct BreedImage: Decodable {
+struct BreedImage: Decodable, Equatable {
     let url: String
 }
 
@@ -24,6 +25,7 @@ extension Breed {
             temperament: "Active, Energetic, Independent, Intelligent, Gentle",
             origin: "Egypt",
             description: "The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals.",
+            referenceImageId: "abys",
             isFavorite: false
         ),
         Breed(
@@ -33,6 +35,7 @@ extension Breed {
             temperament: "Affectionate, Social, Intelligent, Playful, Active",
             origin: "Greece",
             description: "Native to the Greek islands known as the Cyclades in the Aegean Sea, these are natural cats, meaning they developed without humans getting involved in their breeding. As a breed, Aegean Cats are rare, although they are numerous on their home islands. They are generally friendly toward people and can be excellent cats for families with children.",
+            referenceImageId: "aege",
             isFavorite: false
         ),
         Breed(
@@ -42,6 +45,7 @@ extension Breed {
             temperament: "Intelligent, Interactive, Lively, Playful, Sensitive",
             origin: "United States",
             description: "American Bobtails are loving and incredibly intelligent cats possessing a distinctive wild appearance. They are extremely interactive cats that bond with their human family with great devotion.",
+            referenceImageId: "abob",
             isFavorite: false
         )
     ]
